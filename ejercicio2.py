001c108104cadf7e92b4fe64f8827e13abd71b02
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-09-24 09:37:16 +0200

    Commit de prueba para ver que todo esta bien

---
+++ ejercicio2.py
@@ -0,0 +1,12 @@
+#Calcula la suma de dos números ingresados por el usuario. 
+
+num1 = input("Por favor ingrese el primer número: ")
+
+num2 = input("Por favor ingrese el segundo número: ")
+
+num1=int(num1)
+num2=int(num2)
+
+suma=num1+num2
+
+print("El resultado de la suma de "+str(num1)+" y "+str(num2)+" es "+str(suma))
---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+#Calcula el área de un triángulo con la fórmula: Área = (base * altura) / 2. 
+
+base=5
+altura=8
+
+area=(base*altura)/2
+
+print("El area de un triangulo con base "+str(base)+" y "+str(altura)+" es "+str(area))
